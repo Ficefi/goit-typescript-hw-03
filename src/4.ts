471750078e14c07af01d3b6aeccdc8b7bc3d3c9a
Ficefi <murdzamarkian54@gmail.com>
2024-03-14 21:55:43 +0200

    did task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+	private signature: number;
+
+	constructor() {
+		this.signature = Math.random();
+	}
+
+	getSignature() {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {
+		this.key = key;
+	}
+
+	getKey() {
+		return key;
+	}
+}
+
+abstract class House {
+	protected door: boolean;
+	protected tennants: Person[] = [];
+
+	constructor(protected key: Key) {
+		this.door = false;
+		this.key = key;
+	}
+
+	comeIn(person: Person): void {
+		if (this.door === true) {
+			this.tennants.push(person);
+		}
+	}
+
+	abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
